3049405af58b06ecde5c8026503c42daf5b658dc
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-07-20 21:54:57 -0300

    Adicionando evento de click

--- ConversorMoedas/app/src/main/java/com/example/conversormoedas/MainActivity.java
+++ ConversorMoedas/app/src/main/java/com/example/conversormoedas/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.conversormoedas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextClock;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private  ViewHolder mViewHolder = new ViewHolder();
 
@@ -22,8 +23,18 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textEuro = findViewById(R.id.text_Euro);
         this.mViewHolder.buttonCalculate = findViewById(R.id.button_Calculate);
 
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+
 
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button_Calculate ){
+            // logica
+        }
+    }
+
 
     private static class ViewHolder {
         EditText editValue;
